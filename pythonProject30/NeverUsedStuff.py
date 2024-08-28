14c111995d162ff373d6da06092e8842caebfff9
Damirius-compilator <89788569+Damirius-compilator@users.noreply.github.com>
2024-08-28 20:45:33 +0300

    Get Started
    Developer with no skill joined the game
    
    Co-Authored-By: Kynjut <98910865+Kynjut@users.noreply.github.com>

---
+++ pythonProject30/NeverUsedStuff.py
@@ -0,0 +1,18 @@
+import os
+
+
+def create():
+    name = input('note name:\n')
+    with open(f"{name}.txt", "w") as edit:
+        input_text_saves = input('start typing:\n')
+        edit.write(input_text_saves)
+        edit.close()
+
+
+def delete():
+    name = input('enter name of note, you want to delete:\n')
+    os.remove(f'C:\\Users\\admin\\Documents\\CyberClub\\CollabRep\\{name}.txt')
+
+
+create()
+delete()
